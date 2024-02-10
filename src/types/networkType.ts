a1095c13363cfade50419cec5eb82c0bc7762e69
Ferit Kahraman <feritkahraman32135@gmail.com>
2024-02-10 17:32:15 +0330

    chore: remove global network

--- src/types/networkType.ts
+++ src/types/networkType.ts
@@ -1,10 +1,1 @@
-/* eslint-disable @typescript-eslint/no-namespace */
 export type Network = 'mainnet' | 'testnet';
-
-declare global {
-  namespace Express {
-    interface Response {
-      network: Network;
-    }
-  }
-}
