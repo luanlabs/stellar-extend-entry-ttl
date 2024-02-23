8087bfd9318804fd411f604768f2e4cc03bbc791
Ferit Kahraman <feritkahraman32135@gmail.com>
2024-02-23 21:42:40 +0330

    chore: remove getFee file

--- src/utils/soroban/getConfig.ts
+++ src/utils/soroban/getConfig.ts
@@ -1,19 +1,18 @@
 import { Contract, Keypair, SorobanRpc } from 'stellar-sdk';
 
-import getFee from './getFee';
-
 const getConfig = async () => {
   const contract = new Contract(String(process.env.CONTRACT_ID));
   const server = new SorobanRpc.Server(String(process.env.FUTURENET_RPC_URL));
   const adminSecretKey = Keypair.fromSecret(String(process.env.ADMIN_SECRET_KEY));
   const admin = await server.getAccount(adminSecretKey.publicKey());
+  const fee = String(process.env.BASE_FEE) || '1000000000';
 
   return {
     contract,
     server,
     admin,
     adminSecretKey,
-    fee: getFee(),
+    fee,
   };
 };
 export default getConfig;
--- src/utils/soroban/getFee.ts
+++
@@ -1,4 +0,0 @@
-const getFee = (): string => {
-  return String(process.env.BASE_FEE) || '100000';
-};
-export default getFee;
